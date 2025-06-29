ac492213b0a95103635e04bcbb7e724000f6ad0f
rztheguy <160594072+rztheguy@users.noreply.github.com>
2025-06-29 18:58:07 +0000

    fix

--- Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main (String[] args) {
-        // new UI(); // Commented out because UI class does not exist
-        //        new gameUI.UI(); // Commented out because package gameUI does not exist
-    }
-}
